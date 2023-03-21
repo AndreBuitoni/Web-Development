fc6ed2f66466aec2dc5fe9fd441457a8e8d2c5cf
André Buitoni <a.buitoni@gmail.com>
2023-03-21 15:07:15 -0300

    used switch to select currentDay

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,15 +2,43 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
+app.set('view engine', 'ejs')
 
 app.get("/", function(req, res) {
+
     var today = new Date()
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.write("Yay it's the weekend!")
-    } else {
-        res.sendFile(__dirname + "/index.html")
+    var currentDay = today.getDay()
+    var day = ""
+
+    switch (currentDay) {
+      case 0:
+        day = "Sunday";
+        break;
+      case 1:
+        day = "Monday";
+        break;
+      case 2:
+        day = "Tuesday";
+        break;
+      case 3:
+        day = "Wednesday";
+        break;
+      case 4:
+        day = "Thursday";
+        break;
+      case 5:
+        day = "Friday";
+        break;
+      case 6:
+        day = "Saturday";
+        break;
+      default:
+        console.log("Error: current day is equal to " + currentDay)
     }
-    
+
+    res.render("list", {
+        kindOfDay: day
+    })
 })
 
 
