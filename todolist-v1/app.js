d622e4bf3b288f77fbc5f36f48fa41a70ee213d7
André Buitoni <a.buitoni@gmail.com>
2023-03-21 18:36:50 -0300

    Added CSS file

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -7,18 +7,19 @@ var itens = ["Buy Food", "Cook Food", "Eat Food"]
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
 
 app.get("/", function(req, res) {
 
-    var today = new Date()
+    let today = new Date()
     
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     }
 
-    var day = today.toLocaleDateString("en-US", options)
+    let day = today.toLocaleDateString("en-US", options)
 
     res.render("list", {
         kindOfDay: day,
@@ -27,7 +28,7 @@ app.get("/", function(req, res) {
 })
 
 app.post("/", function (req, res) {
-    var item = req.body.newItem
+    let item = req.body.newItem
     itens.push(item)
     res.redirect("/")
 })
