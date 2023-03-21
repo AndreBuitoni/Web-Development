3df356d1c2dcf6954b616ec50b2c245667a8c3cb
André Buitoni <a.buitoni@gmail.com>
2023-03-21 20:36:51 -0300

    Created our module date.js
    And also refactored some code.

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const date = require(__dirname + "/date.js")
 
 const app = express()
 
-let itens = ["Buy Food", "Cook Food", "Eat Food"]
-let workItens = []
+const itens = ["Buy Food", "Cook Food", "Eat Food"]
+const workItens = []
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
@@ -12,15 +13,7 @@ app.use(express.static("public"))
 
 app.get("/", function(req, res) {
 
-    let today = new Date()
-    
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-
-    let day = today.toLocaleDateString("en-US", options)
+    const day = date.getDate()
 
     res.render("list", {
         listTitle: day,
@@ -29,7 +22,7 @@ app.get("/", function(req, res) {
 })
 
 app.post("/", function (req, res) {
-    let item = req.body.newItem
+    const item = req.body.newItem
     if (req.body.list === "Work") {
         workItens.push(item)
         res.redirect("/work")
