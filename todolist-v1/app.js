0746359e99a0df6ab66c13ec0aa25fc36c4b16a0
André Buitoni <a.buitoni@gmail.com>
2023-03-21 15:58:23 -0300

    Finished passing info to server from the webpage

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,46 +2,35 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
+
+var itens = ["Buy Food", "Cook Food", "Eat Food"]
+
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res) {
 
     var today = new Date()
-    var currentDay = today.getDay()
-    var day = ""
-
-    switch (currentDay) {
-      case 0:
-        day = "Sunday";
-        break;
-      case 1:
-        day = "Monday";
-        break;
-      case 2:
-        day = "Tuesday";
-        break;
-      case 3:
-        day = "Wednesday";
-        break;
-      case 4:
-        day = "Thursday";
-        break;
-      case 5:
-        day = "Friday";
-        break;
-      case 6:
-        day = "Saturday";
-        break;
-      default:
-        console.log("Error: current day is equal to " + currentDay)
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
 
+    var day = today.toLocaleDateString("en-US", options)
+
     res.render("list", {
-        kindOfDay: day
+        kindOfDay: day,
+        newListItens: itens
     })
 })
 
-
+app.post("/", function (req, res) {
+    var item = req.body.newItem
+    itens.push(item)
+    res.redirect("/")
+})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000")
