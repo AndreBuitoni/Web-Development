cb6f48ee8af731f160bed795ce8133ec69fa67bc
André Buitoni <a.buitoni@gmail.com>
2023-07-27 13:15:28 -0300

    Migrating from Mongo Driver to Mongoose

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -1,21 +1,20 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://127.0.0.1:27017/';
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true });
 
-// Database name
-const dbName = 'fruitsDB';
-
-// Create a new MongoClient
-const client = new MongoClient(url, { useNewUrlParser: true});
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  client.close();
-});
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Insert some documents
+  collection.insertMany([
+    {name : "Apple", score: 8, review: "Great Fruit"},
+    {name : "Orange", score: 6, review: "Kinda Sour"},
+    {name : "Banana", score: 9, review: "Great Stuff!"}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
